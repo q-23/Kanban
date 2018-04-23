15cbea3ab464a08417057fdc01d61d48e04bcd48
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2018-04-23 22:39:26 +0200

    Add content adding functions and variables

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ $(function() {
 	    }
 	    return str;
 	}
+
+	function Column(name) {
+	    var self = this; // useful for nested functions
+
+	    this.id = randomString();
+	    this.name = name;
+	    this.$element = createColumn();
+
+	    function createColumn() {
+			var $column = $('<div>').addClass('column');
+			var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+			var $columnCardList = $('<ul>').addClass('column-card-list');
+			var $columnDelete = $('<button>').addClass('btn-delete').text('x');
+			var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
+			}
+	  }
 })
